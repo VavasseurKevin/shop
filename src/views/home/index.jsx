f3b3e59091f0e6f900b07e72a7e6de16deaca70d
Kevin Vavasseur <minhluan.phan@gmail.com>
2023-10-30 22:42:59 +0100

    Install sass

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as ROUTES from '../../constants/routes';
+import bannerImg from '../../images/banner-girl.png';
+
 
 const Home = () => {
 
     return (
-        <main>
+        <main className="content">
             <div>
                 <div>
                     <div>
@@ -15,13 +17,31 @@ const Home = () => {
                             <strong>Clarté</strong>
                         </h1>
                         <p>
-                        L'achat de lunettes devrait vous rendre heureux et beau, avec de l'argent en poche.
+                        L&apos;achat de lunettes devrait vous rendre heureux et beau, avec de l&apos;argent en poche.
                         Lunettes, lunettes de soleil et lentilles de contact : nous couvrons vos yeux.
                         </p>
                         <br/>
                         <Link to={ROUTES.SHOP} >
                         Acheter maintenant &nbsp;
                         </Link>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={bannerImg} alt="Banner" />
+                        </div>
+                        <div>
+                            <div>
+                                <h1>Produits Populaires</h1>
+                                <Link to={ROUTES.FEATURED_PRODUCTS}>Voir Tout</Link>
+                            </div>
+                            
+                        </div>
+                        <div>
+                            <div>
+                                <h1>Produits Recommandés</h1>
+                                <Link to={ROUTES.RECOMMENDED_PRODUCTS}>Voir Tout</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
