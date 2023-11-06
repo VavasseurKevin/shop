1451424c56812fc098145671b278a8dd72e988f5
Kevin Vavasseur <minhluan.phan@gmail.com>
2023-11-06 17:34:33 +0100

    Add Recommended Products view

--- src/views/recommended/index.jsx
+++ src/views/recommended/index.jsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { ProductShowcaseGrid } from "../../components/product";
+import bannerImgWoman from '../../images/banner-woman.png';
 
-const RecommendedProducts = () => {
+const RecommendedProducts = () => {return (
+    <main className="content">
+        <div className="featured">
+            <div className="banner">
+                <div className="banner-desc">
+                    <h1>Produits Recommandées</h1>
+                </div>
+                <div className="banner-img">
+                    <img src={bannerImgWoman} alt="" />
+                </div>
+            </div>
+            <div className="display">
+                <div className="product-display-grid">
+                    <ProductShowcaseGrid/>
+                </div>
+            </div>
+        </div>
+    </main>
+
+)
 
 }
 
