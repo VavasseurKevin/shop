9244e11ca5aa780ece8df11b75a6691d8a25c59b
Kevin Vavasseur <minhluan.phan@gmail.com>
2023-11-08 17:26:34 +0100

    Fix style form SignUp view

--- src/views/auth/signup/index.jsx
+++ src/views/auth/signup/index.jsx
@@ -5,6 +5,7 @@ import { SocialLogin } from "../../../components/common";
 
 const SignUp = () => {
   return (
+    <main className="content">
     <div className="auth-content">
       <div className="auth">
         <div className="auth-main">
@@ -19,33 +20,48 @@ const SignUp = () => {
             {() => (
               <Form>
                 <div className="auth-field">
-                  <label htmlFor="nomprenom">* Nom & Prénom</label>
-                  <br/>
-                  <Field
-                    name="nomprenom"
-                    type="text"
-                    placeholder="Kévin Vavasseur"
-                    style={{ textTransform: "capitalize" }}
-                  />
+                  <div className="input-group">
+                    <label className="label-input" htmlFor="nomprenom">
+                      * Nom & Prénom
+                    </label>
+                    <br />
+                    <Field
+                      name="nomprenom"
+                      type="text"
+                      placeholder="Kévin Vavasseur"
+                      style={{ textTransform: "capitalize" }}
+                      className="input-form input-error"
+                    />
+                  </div>
                 </div>
                 <div className="auth-field">
-                  <label htmlFor="email">* Email</label>
-                  
-                  <br/>
-                  <Field
-                    name="email"
-                    type="email"
-                    placeholder="test@exemple.com"
-                  />
+                  <div className="input-group">
+                    <label className="label-input" htmlFor="email">
+                      * Email
+                    </label>
+
+                    <br />
+                    <Field
+                      name="email"
+                      type="email"
+                      placeholder="test@exemple.com"
+                      className="input-form input-error"
+                    />
+                  </div>
                 </div>
                 <div className="auth-field">
-                  <label htmlFor="password">* Mot de passe</label>
-                  <br/>
-                  <Field
-                    name="password"
-                    type="password"
-                    placeholder="Mot de passe"
-                  />
+                  <div className="input-group">
+                    <label className="label-input" htmlFor="password">
+                      * Mot de passe
+                    </label>
+                    <br />
+                    <Field
+                      name="password"
+                      type="password"
+                      placeholder="Mot de passe"
+                      className="input-form input-error"
+                    />
+                  </div>
                 </div>
                 <br />
                 <div className="auth-field auth-action auth-action-signup">
@@ -58,11 +74,23 @@ const SignUp = () => {
           </Formik>
         </div>
         <div className="auth-divider">
-              <h6>OR</h6>
-            </div>
-            <SocialLogin/>
+          <h6>OR</h6>
+        </div>
+        <SocialLogin />
+      </div>
+      <div className="auth-message">
+        <span className="auth-info">
+          <strong>Vous avez déjà un compte ?</strong>
+        </span>
+        <button
+          className="button button-small button-border button-border-gray"
+          type="button"
+        >
+          Se connecter
+        </button>
       </div>
     </div>
+    </main>
   );
 };
 
