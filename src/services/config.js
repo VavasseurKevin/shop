998ccfe779f2eb082ef4b429869d97897bca50e8
Kevin Vavasseur <minhluan.phan@gmail.com>
2023-11-10 15:56:56 +0100

    Create services/config.js and firebase.js

---
+++ src/services/config.js
@@ -0,0 +1,16 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmPEOvxuIHBPjgrvgY69dxsAybtfdOQhs",
+  authDomain: "ma-vision-ecommerce.firebaseapp.com",
+  projectId: "ma-vision-ecommerce",
+  storageBucket: "ma-vision-ecommerce.appspot.com",
+  messagingSenderId: "486837500368",
+  appId: "1:486837500368:web:5c8965c9467122e10fa1d5"
+};
+
+
+
+export default firebaseConfig;
